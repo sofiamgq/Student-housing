f570398e6e94fd97c2ec9b3526626254675d47a3
lowerthanu <32088658+ForeverDog@users.noreply.github.com>
2022-12-13 15:42:18 +0000

    fixed stuff

--- faculty_initialization.py
+++ faculty_initialization.py
@@ -15,6 +15,39 @@ def facultyFullName(facultyID):
   """This function combines the first name and the surname to get the full name."""
   return db[facultyID]['facultyName'] + ' ' + db[facultyID]['facultySurname']
 
-def logout():
-  """This function logs out the user of their account."""
-  print('You have successfully logged out.')
+def addNewFaculty(currentUser):
+  """This function allows faculty members to add more faculty members if they are not already in the system."""
+  login = input('Choose a login: ')
+  indicator = False
+  while indicator == False:
+    flag = True
+    for elem in db.keys():
+      if 'Faculty' in elem:
+        if db[elem]['facultyLogin'] == login:
+          flag = False
+          break
+    if flag == False:
+      print('Such login already exsists.')
+      login = input('Choose a login: ')
+    else:
+      indicator = True
+  print ('Your password MUST cobtain at least 8 characters, 1 uppercase letter, 1 number and 1 special character')
+  password = pwinput.pwinput(prompt='Enter your password: ', mask='*')
+  #print ('here')
+  if condPassword(password):
+    #print ('got here')
+    verification =  pwinput.pwinput(prompt='Repeat your password: ', mask='*')
+    while password != verification:
+      print('The passwords do not match. Try again.')
+      password = pwinput.pwinput(prompt='Enter your password: ', mask='*')
+      verification =  pwinput.pwinput(prompt='Repeat your password: ', mask='*')
+  else:
+    print ('Your password does not fulfill the requirements. Please try again.')
+    addNewFaculty(currentUser)
+  facultyName = input('Enter new faculty member\'s name: ')
+  facultySurname = input('Enter new faculty member\'s surname: ')
+  facultyID = 'Faculty' + str(db['NumOfFac'] + 1)
+  initializeFaculty(login, password, facultyName, facultySurname, facultyID)
+  db['NumOfFac'] += 1
+  print('A new faculty member, ' + facultyFullName(facultyID) + ', was successfully added to the system.')
+  facultyPage(currentUser)
--- main.py
+++ main.py
@@ -125,43 +125,6 @@ def studentPage(currentUser):
   elif studentPageInput == '4':
     logout()
 
-def addNewFaculty(currentUser):
-  """This function allows faculty members to add more faculty members if they are not already in the system."""
-  login = input('Choose a login: ')
-  indicator = False
-  while indicator == False:
-    flag = True
-    for elem in db.keys():
-      if 'Faculty' in elem:
-        if db[elem]['facultyLogin'] == login:
-          flag = False
-          break
-    if flag == False:
-      print('Such login already exsists.')
-      login = input('Choose a login: ')
-    else:
-      indicator = True
-  print ('Your password MUST cobtain at least 8 characters, 1 uppercase letter, 1 number and 1 special character')
-  password = pwinput.pwinput(prompt='Enter your password: ', mask='*')
-  #print ('here')
-  if condPassword(password):
-    #print ('got here')
-    verification =  pwinput.pwinput(prompt='Repeat your password: ', mask='*')
-    while password != verification:
-      print('The passwords do not match. Try again.')
-      password = pwinput.pwinput(prompt='Enter your password: ', mask='*')
-      verification =  pwinput.pwinput(prompt='Repeat your password: ', mask='*')
-  else:
-    print ('Your password does not fulfill the requirements. Please try again.')
-    addNewFaculty(currentUser)
-  facultyName = input('Enter new faculty member\'s name: ')
-  facultySurname = input('Enter new faculty member\'s surname: ')
-  facultyID = 'Faculty' + str(db['NumOfFac'] + 1)
-  initializeFaculty(login, password, facultyName, facultySurname, facultyID)
-  db['NumOfFac'] += 1
-  print('A new faculty member, ' + facultyFullName(facultyID) + ', was successfully added to the system.')
-  facultyPage(currentUser)
-
 def facultyPage(currentUser):
   """This function is the faculty portal, and it asks the user what they want to do such as managing students or adding nee facukty members."""
   print('Enter 1 to access student information\nEnter 2 to add a new faculty member\nEnter 3 to get a housing plan\nEnter 4 to log out')
@@ -177,6 +140,10 @@ def facultyPage(currentUser):
     generateHousingAssignments()
   elif facultyPageInput == '4':
     logout()
+
+def logout():
+  """This function logs out the user of their account."""
+  print('You have successfully logged out.')
 
 def mainscreen():
   """This function is the main screen, which is the first page that users visualize, which asks them to either log in or sign up."""
